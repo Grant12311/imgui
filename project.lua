3ba254a6794d6c1a474de3c4823b2726a7da701b
Grant Joseph <grantj150@yahoo.com>
2020-11-01 00:37:23 -0400

    add -w build option

--- project.lua
+++ project.lua
@@ -5,4 +5,6 @@ project("imgui")
     targetdir("bin/%{cfg.platform}/%{cfg.buildcfg}")
     objdir("obj/%{cfg.platform}/%{cfg.buildcfg}")
 
+    buildoptions({"-w"})
+
     files({"*.h", "*.cpp", "backends/imgui_impl_opengl3.cpp", "backends/imgui_impl_opengl3.h", ".gitignore", "project.lua"})
