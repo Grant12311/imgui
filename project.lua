2fe587ba8ef8da925c00631ee514247f84f3b8af
Grant Joseph <granetejo@gmail.com>
2020-12-29 16:09:50 -0500

    add files for glfw backend

--- project.lua
+++ project.lua
@@ -7,4 +7,4 @@ project("imgui")
 
     buildoptions({"-w"})
 
-    files({"*.h", "*.cpp", "backends/imgui_impl_opengl3.cpp", "backends/imgui_impl_opengl3.h", ".gitignore", "project.lua"})
+    files({"*.h", "*.cpp", "backends/imgui_impl_opengl3.cpp", "backends/imgui_impl_opengl3.h", "backends/imgui_impl_glfw.cpp", "backends/imgui_impl_glfw.h", ".gitignore", "project.lua"})
